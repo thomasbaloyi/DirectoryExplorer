94ee7d7ed190ff5d9ff6fb6509af500decffcb8a
Thomas Baloyi <baloyi.thomas@outlook.com>
2023-05-31 21:44:36 +0200

    Communicate with backend

--- frontend/src/app/components/search/search.component.ts
+++ frontend/src/app/components/search/search.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Directory } from 'src/app/model/directory.model';
+import { ExplorerService } from 'src/app/services/explorer.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+
+  directories: Directory[] = [];
+
+  constructor(private explorerService: ExplorerService) {}
+
+  ngOnInit(): void {
+    this.getDirectoryDetails(`C:\\Projects`);
+  }
+
+  public getDirectoryDetails(directory: string): void {
+    this.explorerService.getDirectory(directory).subscribe((response: Directory[]) => {
+      this.directories =response;
+    });
+  }
 
 }
--- frontend/src/app/services/explorer.service.ts
+++ frontend/src/app/services/explorer.service.ts
@@ -2,15 +2,22 @@ import { Injectable } from '@angular/core';
 import { Directory } from '../model/directory.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ExplorerService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  public getDirectory(path: string): Observable<Directory> {
-    return this.http.get<Directory>('');
+  /**
+   * Get directory details
+   * @param name - name of the direcotry
+   * @returns directory details and contents
+   */
+  public getDirectory(name: string): Observable<Directory[]> {
+    return this.http.get<Directory[]>(
+      `${environment.backendUrl}?directoryPath=${name}`
+    );
   }
 }
